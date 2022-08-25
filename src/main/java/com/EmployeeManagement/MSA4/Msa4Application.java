5550db9defb45c1d5dcf1a02de579dc2e3381423
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-25 10:53:54 +0530

    First Commit Spring Boot Maven Project Created using Initiazlizer(Empty Project)

---
+++ src/main/java/com/EmployeeManagement/MSA4/Msa4Application.java
@@ -0,0 +1,13 @@
+package com.EmployeeManagement.MSA4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msa4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmployeeManagement/MSA4/Msa4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeeManagement.MSA4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msa4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
